1d0c139f5936e172bd202b63c0dbef1a193f31e6
Chimo-byte <sundayadeniyi47@gmail.com>
2026-08-26 14:36:31 +0100

    Add files via upload

--- auto_fetch.py
+++ auto_fetch.py
@@ -7,9 +7,9 @@ API_KEY = os.environ.get("NEWS_API_KEY")
 
 # Define the categories you want to fetch daily
 CATEGORIES = [
-    {"api_name": "technology", "display_name": "Technology"},
-    {"api_name": "business", "display_name": "Business"},
-    {"api_name": "entertainment", "display_name": "Culture"}
+    {"api_name": "technology", "display_name": "Tech Media"},
+    {"api_name": "business", "display_name": "Wordld News"},
+    {"api_name": "entertainment", "display_name": "Cinema review"}
 ]
 
 today_date = datetime.datetime.now().strftime("%Y-%m-%d")
