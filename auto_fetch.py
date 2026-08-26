160e9af6d7375dbb319e2f02c9ae7addc8cf3000
Chimo-byte <sundayadeniyi47@gmail.com>
2026-08-26 15:09:53 +0100

    auto_fetch.py

--- auto_fetch.py
+++ auto_fetch.py
@@ -9,7 +9,7 @@ API_KEY = os.environ.get("NEWS_API_KEY")
 CATEGORIES = [
     {"api_name": "technology", "display_name": "Tech & Media"},
     {"api_name": "business", "display_name": "World News"},
-    {"api_name": "entertainment", "display_name": "Cinema & Reviews"}
+    {"api_name": "Soccer", "display_name": "Soccer"}
 ]
 
 today_date = datetime.datetime.now().strftime("%Y-%m-%d")
